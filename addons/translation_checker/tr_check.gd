0dc984526fb9a90665166ac481de7a322a3c3c41
night-glider <nightgliderdev@gmail.com>
2023-07-11 19:02:58 +0300

    Initial commit

---
+++ addons/translation_checker/tr_check.gd
@@ -0,0 +1,102 @@
+tool
+extends EditorPlugin
+
+func _enter_tree():
+	add_tool_menu_item("Translation Check", self, "check_translation")
+
+func _exit_tree():
+	remove_tool_menu_item("Translation Check")
+
+func check_tags(line):
+	var current_tag = null
+	for chr in line:
+		
+		if chr == "[":
+			if current_tag != null:
+				return false
+			current_tag = "["
+			continue
+		
+		if chr == "]":
+			if current_tag != "[":
+				return false
+			current_tag = null
+			continue
+	
+	if current_tag != null:
+		return false
+	return true
+
+func check_translation(args):
+	var all_keys_in_scenes:Array = []
+	var all_keys_in_scenes_paths:Array = []
+	var all_keys_in_translation:Array = []
+	
+	var regex = RegEx.new()
+	regex.compile("\"[A-Z_][A-Z0-9_]{1,100}\"")
+	var files_list:Array = get_all_files("res://")
+	for element in files_list:
+		var file = File.new()
+		file.open(element, File.READ)
+		var content = file.get_as_text()
+		file.close()
+		
+		var result:Array = regex.search_all(content)
+		for matches in result:
+			all_keys_in_scenes.append( matches.get_string().rstrip("\"").lstrip("\"") )
+			all_keys_in_scenes_paths.append(element)
+	
+	var file := File.new()
+	file.open("res://translation/translation_table.csv", File.READ)
+	file.get_csv_line()
+	while file.get_position() < file.get_len():
+		all_keys_in_translation.append( file.get_csv_line()[0] )
+	
+	var error_message:String = ""
+	for i in all_keys_in_scenes.size():
+		if not all_keys_in_scenes[i] in all_keys_in_translation:
+			error_message += ("ERROR KEY DOES NOT EXITS: " + all_keys_in_scenes[i]) + "\n"
+			error_message += ("PATH: " + all_keys_in_scenes_paths[i]) + "\n\n"
+	
+	for element in all_keys_in_translation:
+		if not element in all_keys_in_scenes:
+			error_message += ("KEY " + element + " EXISTS BUT IS NOT USED" ) + "\n"
+	
+	print(error_message)
+	OS.alert(error_message)
+	
+	error_message = check_close_tags()
+	print(error_message)
+	OS.alert(error_message)
+
+func get_all_files(path: String):
+	var dir = Directory.new()
+	var result:Array = []
+	
+	if dir.open(path) == OK:
+		dir.list_dir_begin(true, true)
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				result.append_array( get_all_files(dir.get_current_dir().plus_file(file_name) ) )
+			else:
+				if file_name.get_extension() in ["tscn", "gd"]:
+					result.append( dir.get_current_dir().plus_file(file_name) )
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access %s." % path)
+	return result
+
+func check_close_tags():
+	var errors = "--tags errors--\n"
+	var file:File = File.new()
+	
+	file.open("res://translation/translation_table.csv", File.READ)
+	file.get_csv_line()
+	while file.get_position() < file.get_len():
+		var lines = file.get_csv_line()
+		for line in lines:
+			if not check_tags(line):
+				errors += line + "\n"
+	
+	return errors
---
+++ scenes/dialogue_debugger.gd
@@ -0,0 +1,81 @@
+extends Control
+
+const translation_table := preload("res://translation/translation_table.en.translation")
+onready var key_list := $search_panel/VBoxContainer/ItemList
+
+var uncompressed_dict := {}
+
+func _ready():
+	for key in translation_table.get_message_list():
+		uncompressed_dict[key] = tr(key)
+	show_full_list()
+
+func check_tags(line):
+	var current_tag = null
+	for chr in line:
+		
+		if chr == "[":
+			if current_tag != null:
+				return false
+			current_tag = "["
+			continue
+		
+		if chr == "]":
+			if current_tag != "[":
+				return false
+			current_tag = null
+			continue
+	
+	if current_tag != null:
+		return false
+	return true
+
+func show_full_list():
+	key_list.clear()
+	for key in uncompressed_dict:
+		key_list.add_item(key)
+	key_list.sort_items_by_text()
+
+func _on_ItemList_item_activated(index):
+	var key = key_list.get_item_text(index)
+	$edit_panel/TextEdit.text = tr(key)
+	_on_TextEdit_text_changed()
+
+
+func _on_TextEdit_text_changed():
+	var text = $edit_panel/TextEdit.text
+	if check_tags(text):
+		$dialogue_panel/DialogueLabel.messages = [text]
+	else:
+		$dialogue_panel/DialogueLabel.messages = ["[spd 1][color=red]ERROR incorrect tag"]
+	$dialogue_panel/DialogueLabel.start_dialogue()
+
+
+func _on_find_keys_pressed():
+	key_list.clear()
+	var search_word = $search_panel/VBoxContainer/LineEdit.text
+	if search_word == "":
+		show_full_list()
+		return
+	for key in uncompressed_dict:
+		if search_word.to_lower() in key.to_lower():
+			key_list.add_item(key)
+	
+	key_list.sort_items_by_text()
+
+func _on_find_values_pressed():
+	key_list.clear()
+	var search_word = $search_panel/VBoxContainer/LineEdit.text
+	if search_word == "":
+		show_full_list()
+		return
+	for key in uncompressed_dict:
+		if search_word.to_lower() in tr(key).to_lower():
+			key_list.add_item(key)
+	
+	key_list.sort_items_by_text()
+
+
+func _on_DialogueLabel_dialogue_custom_event(data):
+	$event_dialogue.dialog_text = "event emitted!\n" + data
+	$event_dialogue.visible = true
